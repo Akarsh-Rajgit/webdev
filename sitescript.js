c5e208c6791348526c655c8c7dc5e29485b1ecd4
Akarsh-Rajgit <95570274+Akarsh-Rajgit@users.noreply.github.com>
2025-12-31 10:24:50 +0530

    Delete sitescript.js

--- sitescript.js
+++
@@ -1,6 +0,0 @@
-function toggleImage(event) {
-            let img = event.currentTarget.querySelector(".service-img");
-            if (img) {
-                img.style.display = img.style.display === "block" ? "none" : "block";
-            }
-        }
