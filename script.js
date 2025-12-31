743a2a5d9cb35bab46e9308691d96b80abaae5a9
Akarsh-Rajgit <95570274+Akarsh-Rajgit@users.noreply.github.com>
2025-12-31 10:25:38 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleDetails(id) {
-            var details = document.getElementById(id);
-            if (details.style.display === "block") {
-                details.style.display = "none";
-            } else {
-                details.style.display = "block";
-            }
-        }
